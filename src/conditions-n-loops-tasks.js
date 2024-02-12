3bd97e10239f9a4958165a8dffe1ec96134aa8e7
Denis Saveliev <denissaveliev23@gmail.com>
2024-02-12 09:31:30 +0200

    feat: solve next task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -67,8 +67,10 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const difX = Math.abs(queen.x - king.x);
+  const difY = Math.abs(queen.y - king.y);
+  return difX === 0 || difY === 0 || difX === difY;
 }
 
 /**
